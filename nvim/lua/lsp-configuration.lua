f6bd5b0e2ff32bb44a6437a683eda5503a949b6c
donny <dongook.son@yonsei.ac.kr>
2022-10-08 07:49:50 +0900

    add tsserver and modify vue snippet

--- nvim/lua/lsp-configuration.lua
+++ nvim/lua/lsp-configuration.lua
@@ -115,10 +115,10 @@ require 'lspconfig'.bashls.setup {
 }
 
 -- typescript >> npm install -g typescript typescript-language-server
--- require 'lspconfig'.tsserver.setup {
---   capabilities = capabilities,
---   on_attach = on_attach,
--- }
+require 'lspconfig'.tsserver.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+}
 
 -- solidity >> npm i -g solidity-language-server
 require 'lspconfig'.solidity_ls.setup {
