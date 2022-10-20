46245f983556f02e317d5bf85efff00b6e26e0ed
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-10-20 17:50:37 +0900

    udpate wezterm config: looks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,17 @@
 local wezterm = require 'wezterm';
 return {
   font = wezterm.font({
-    family = "Hurmit Nerd Font Mono",
+    family = "VictorMono Nerd Font",
   }),
-  font_size = 18,
-  color_scheme = "Sequoia Moonlight",
+  font_size = 15,
+  color_scheme = "Galizur",
   default_cursor_style = "BlinkingBlock",
   cursor_blink_ease_in = "Linear",
   cursor_blink_ease_out = "Linear",
-  cursor_blink_rate = 1000,
+  cursor_blink_rate = 500,
   animation_fps = 60,
   audible_bell = "Disabled",
   enable_tab_bar = false,
-  line_height = 1.2,
+  line_height = 1.3,
   warn_about_missing_glyphs = false,
 }
