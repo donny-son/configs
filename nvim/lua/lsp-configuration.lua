ad79af0cfd308236fe5d8fad10c6205493f8d9e7
Dongook Son <60206749+donny-son@users.noreply.github.com>
2023-01-29 23:32:55 +0900

    Ruff not working detected but not attached to buffer

--- nvim/lua/lsp-configuration.lua
+++ nvim/lua/lsp-configuration.lua
@@ -12,22 +12,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
 		bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
 		bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+		bufmap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
 		bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
 		bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
 		bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
 		bufmap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-		bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
-		bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-		bufmap("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
+		bufmap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
+		bufmap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>")
 		bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 		bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 		bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 	end,
 })
 
----
--- Diagnostics
----
+-----------------
+-- Diagnostics --
+-----------------
 
 local sign = function(opts)
 	vim.fn.sign_define(opts.name, {
@@ -131,6 +131,7 @@ require("lspconfig").jedi_language_server.setup({
 	root_dir = pyproject_root,
 })
 require("lspconfig").pyright.setup({})
+-- require("lspconfig").ruff_lsp.setup({})
 require("lspconfig").vimls.setup({})
 require("lspconfig").taplo.setup({})
 require("lspconfig").tailwindcss.setup({})
