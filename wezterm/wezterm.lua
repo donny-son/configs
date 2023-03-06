5231231cae7fb0fa679eb3bc8cee4f75331f6eed
Dongook Son <60206749+donny-son@users.noreply.github.com>
2023-03-06 12:47:45 +0900

    Update wezterm and alacritty settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,17 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 return {
-  font = wezterm.font({
-    family = "VictorMono Nerd Font",
-  }),
-  font_size = 12,
-  color_scheme = "Galizur",
-  default_cursor_style = "BlinkingBlock",
-  cursor_blink_ease_in = "Linear",
-  cursor_blink_ease_out = "Linear",
-  cursor_blink_rate = 300,
-  animation_fps = 120,
-  audible_bell = "Disabled",
-  enable_tab_bar = true,
-  line_height = 1.3,
-  warn_about_missing_glyphs = false,
+	font = wezterm.font({
+		family = "FiraCode NF",
+	}),
+	font_size = 15,
+	color_scheme = "Galizur",
+	default_cursor_style = "BlinkingBlock",
+	cursor_blink_ease_in = "Linear",
+	cursor_blink_ease_out = "Linear",
+	cursor_blink_rate = 400,
+	animation_fps = 100,
+	audible_bell = "Disabled",
+	enable_tab_bar = true,
+	line_height = 1.1,
+	warn_about_missing_glyphs = false,
 }
