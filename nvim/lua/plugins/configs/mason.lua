73558df4c2b6f19d95c40e0ebbb37b5e27f74fee
Dongook Son <60206749+donny-son@users.noreply.github.com>
2023-08-31 10:46:17 +0900

    update wezterm, alacritty, astronvim

--- nvim/lua/plugins/configs/mason.lua
+++ nvim/lua/plugins/configs/mason.lua
@@ -1,12 +1,18 @@
 return function(_, opts)
   require("mason").setup(opts)
 
-  -- TODO AstroNvim v4: change these auto command names to not conflict with core Mason commands
+  -- TODO: AstroNvim v4: change these auto command names to not conflict with core Mason commands
   local cmd = vim.api.nvim_create_user_command
   cmd("MasonUpdate", function(options) require("astronvim.utils.mason").update(options.fargs) end, {
     nargs = "*",
     desc = "Update Mason Package",
-    complete = "custom,v:lua.mason_completion.available_package_completion",
+    complete = function(arg_lead)
+      local _ = require "mason-core.functional"
+      return _.sort_by(
+        _.identity,
+        _.filter(_.starts_with(arg_lead), require("mason-registry").get_installed_package_names())
+      )
+    end,
   })
   cmd(
     "MasonUpdateAll",
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,26 @@
-local wezterm = require("wezterm")
-return {
-	font = wezterm.font({
-		family = "FiraCode NF",
-	}),
-	font_size = 15,
-	color_scheme = "Galizur",
-	default_cursor_style = "BlinkingBlock",
-	cursor_blink_ease_in = "Linear",
-	cursor_blink_ease_out = "Linear",
-	cursor_blink_rate = 400,
-	animation_fps = 100,
-	audible_bell = "Disabled",
-	enable_tab_bar = true,
-	line_height = 1.1,
-	warn_about_missing_glyphs = false,
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+config.font = wezterm.font("FiraCode NF")
+config.font_size = 15
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
 }
+
+return config
